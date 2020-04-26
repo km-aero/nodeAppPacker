0da48ca83929ce74ba3ed90266e5645e2ac888c9
Kevin Monteiro <kevin.monteiro96@gmail.com>
2020-04-26 22:50:50 +0100

    deleted provision.sh file, updated packer.json

--- provision.sh
+++
@@ -1,8 +0,0 @@
-sudo systemctl start nginx
-unlink /etc/nginx/sites-enabled/default
-sudo mv /home/ubuntu/reverse-proxy.conf /etc/nginx/sites-available/reverse-proxy.conf
-ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
-sudo systemctl reload-or-restart nginx
-cd /home/ubuntu/app
-npm install
-npm start &
